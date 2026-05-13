f5e16476bf7dfdaff5241f50b923b01b6afa9861
Daan Donders <d.donders1998@gmail.com>
2026-05-13 22:31:43 +0200

    Installed testing components and set up the environment and built and ran the first test succesfully.

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    resolve: {
+        tsconfigPaths: true,
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./vitest.setup.ts",
+    },
+});
